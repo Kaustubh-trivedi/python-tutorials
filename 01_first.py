7cdcfbadbbe5a08e6f758d460a088425256f96dc
indialearnxweb <animeuniversexplore@gmail.com>
2026-03-11 10:19:20 +0530

    init-comments

---
+++ 01_first.py
@@ -0,0 +1,17 @@
+print(20+5,"HEllo world")
+
+
+# Escape Sequence Characters
+
+# print("hey
+# there
+# what 
+# should
+# i
+# do?")
+
+
+print("hey\n there\n what\n should\n i\n do?")
+
+
+#comments  ==> Start with #

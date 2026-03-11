f6a16d568a2a09174548286d2a79abdbbda70d0e
indialearnxweb <animeuniversexplore@gmail.com>
2026-03-11 10:51:39 +0530

    datatypes

---
+++ 02_variable.py
@@ -0,0 +1,63 @@
+a=1
+b="harry"
+c=True
+d=None
+print(a,b,c,d);
+# print(a+b)    ===> Error, type mismatch
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+# data type in pyhton
+# 1. Numeric data: int, float, complex
+# int: 3, -8, 0
+# float: 7.349, -9.0, 0.0000001
+# complex: 6 + 2i
+
+
+
+# 2. Text data: str
+# str: "Hello World!!!", "Python Programming"
+
+
+# 3. Boolean data:
+# Boolean data consists of values True or False.
+
+
+# 4. Sequenced data: list, tuple
+# list: A list is an ordered collection of data with elements separated by a comma and enclosed within square brackets. Lists are mutable and can be modified after creation.
+
+# Example:
+
+# list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+# print(list1)
+# Output:
+
+# [8, 2.3, [-4, 5], ['apple', 'banana']]
+
+
+# Tuple: A tuple is an ordered collection of data with elements separated by a comma and enclosed within parentheses. Tuples are immutable and can not be modified after creation.
+
+# Example:
+
+# tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+# print(tuple1)
+# Output:
+
+# (('parrot', 'sparrow'), ('Lion', 'Tiger'))
+
+
+
+# 5. Mapped data: dict
+# dict: A dictionary is an unordered collection of data containing a key:value pair. The key:value pairs are enclosed within curly brackets.
+
+# Example:
+
+# dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+# print(dict1)
+# Output:
+
+# {'name': 'Sakshi', 'age': 20, 'canVote': True}

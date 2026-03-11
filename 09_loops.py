81277e072f3f1e33706a38bed49fc81eb2a0aa80
indialearnxweb <animeuniversexplore@gmail.com>
2026-03-11 13:27:29 +0530

    loo[p

---
+++ 09_loops.py
@@ -0,0 +1,19 @@
+name="Abhishek"
+for i in name:
+    print(i)
+
+colors = ["Red","green","blue"]
+for color in colors:
+    print(color)
+
+
+for k in range(5):
+    print(k) # 0 to 4
+
+for k in range(1,9):
+    print(k)  # 1 to 8
+print("While lopp")
+i=0
+while(i<3):
+    print(i)
+    i=i+1

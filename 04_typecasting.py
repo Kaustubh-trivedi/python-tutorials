8c459e7247d67d89388c2d9d69a52bf5ba0c6641
indialearnxweb <animeuniversexplore@gmail.com>
2026-03-11 12:20:37 +0530

    typecasting

---
+++ 04_typecasting.py
@@ -0,0 +1,16 @@
+a="1"
+b="2"
+print(a+b)    #12
+print(int(a)+int(b))  #3
+
+#conversion of one data type in another data type is known as type casting
+#two types
+#explicit   --> We do.
+#implicit  --> by default by python
+
+
+
+#implcit
+c=1.9
+d=8  #converted into float
+print(c+d)

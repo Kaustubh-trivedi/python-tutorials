47ebcf7fbbf0cb289fc71f8e710a92b16d422672
indialearnxweb <animeuniversexplore@gmail.com>
2026-03-11 13:18:38 +0530

    match case

---
+++ 08_matchcase.py
@@ -0,0 +1,10 @@
+x=6
+match x:
+    case 0:
+        print("Nah, you are small")
+    case 4:
+        print("Ohh you are equal")
+    case 6:
+        print("What, you are big")
+    case _:
+        print("Nothing that can match")
